5311e2052bb87ffb593fa3b6c93a40936051d441
luckkalpna <luckkalpna@gmail.com>
2025-06-17 19:31:21 +0530

    Handle contact form and store data in db

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -19,4 +21,11 @@ def services(request):
 
 def contact(request):
   # return HttpResponse("this is an contact page")
+  if request.method == "POST":
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    desc = request.POST.get('desc')
+    contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+    contact.save()
   return render(request, 'contact.html')
