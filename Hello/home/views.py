2a972910317aca0e662fd289eb1e2f93112ba2cb
luckkalpna <luckkalpna@gmail.com>
2025-06-17 20:39:05 +0530

    Add a flash message after submission form

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
     'variable1': "Hello",
     'variable2': "Welcome"
   }
+  messages.success(request, "This is a test message")
   # return HttpResponse("this is an home page")
   return render(request, 'index.html', context)
 
@@ -28,4 +30,5 @@ def contact(request):
     desc = request.POST.get('desc')
     contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
     contact.save()
+    messages.success(request, "Your message has been sent.")
   return render(request, 'contact.html')
