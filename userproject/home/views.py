ae4157590406ad3df778419838f57c627b3fcef5
luckkalpna <luckkalpna@gmail.com>
2025-06-19 22:29:30 +0530

    Add login logic for logged in user

--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+
 # password for kalpana user: admin***000
 
 # Create your views here.
@@ -7,6 +9,17 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    if request.method=="POST":
+        user = authenticate(username=username, password=password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if user is not None:
+            return redirect('/')
+        # A backend authenticated the credentials
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
     # return HttpResponse("This is the Login Page")
     return render(request, 'login.html')
 
