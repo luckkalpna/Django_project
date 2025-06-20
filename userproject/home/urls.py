4b9d1a52626cff28dead6f2d3990577261d151ef
luckkalpna <luckkalpna@gmail.com>
2025-06-20 23:32:51 +0530

    check anonymes use and set login, logout function

--- userproject/home/urls.py
+++ userproject/home/urls.py
@@ -5,5 +5,5 @@ from home import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logoutUser, name='logout')
 ]
--- userproject/home/views.py
+++ userproject/home/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+
 
 # password for kalpana user: admin***000
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
     # return HttpResponse("Welcome to the Home Page")
     return render(request, 'index.html')
 
@@ -23,6 +27,7 @@ def login(request):
     # return HttpResponse("This is the Login Page")
     return render(request, 'login.html')
 
-def logout(request):
+def logoutUser(request):
+    logout(request)
     # return HttpResponse("You have been logged out")
-    return render(request, 'logout.html')
+    return redirect('/login')
